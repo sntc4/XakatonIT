fd4745252c4809f466d4bed75214e6abf7892e34
sntc4 <Egor.Borisov@unilever.com>
2020-02-22 18:52:40 +0600

    Создал файл для бд парки

---
+++ Xakaton/Models/Parks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xakaton.Models
+{
+    public class Parks
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } //Название парка
+        public int Address { get; set; } //Адрес парка
+        public int Area { get; set; } //Размер парка
+        public double Rating { get; set; } //Рэйтинг парка
+        
+    }
+}
